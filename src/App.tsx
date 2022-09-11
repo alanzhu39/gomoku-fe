768f3015ddbc3163ff0b433a925dbe3903623419
Alan Zhu <alan.zhu39@gmail.com>
2022-09-10 17:37:28 -0700

    testing http requests with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+/* eslint-disable */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends React.Component {
+class App extends React.Component<any, {message: string}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/hey').then((response) => {
+      this.setState({
+        message: response.data
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +28,7 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
+          <p>{this.state.message}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
