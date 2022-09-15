313d00efc7b114c01075fb5d81005a305274e538
Alan Zhu <alan.zhu39@gmail.com>
2022-09-14 19:20:01 -0700

    switching back to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
 /* eslint-disable */
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-class App extends React.Component<any, {message: string}> {
-  ws: WebSocket;
-  inputRef: React.RefObject<HTMLInputElement>;
+function App() {
+  const [message, setMessage] = useState('');
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      message: ''
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-
-    this.ws = new WebSocket("ws://localhost:8080/lobby/create");
-    this.inputRef = React.createRef<HTMLInputElement>();
-  }
-
-  handleClick() {
-    if (this.inputRef.current?.value) {
-      this.ws.send(this.inputRef.current.value);
+  function handleClick() {
+    if (inputRef.current?.value) {
+      ws.send(inputRef.current.value);
     }
   }
 
-  componentDidMount() {
-    this.ws.onmessage = (event) => {
-      this.setState({
-        message: event.data
-      })
+  const ws = new WebSocket("ws://localhost:8080/lobby/create");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    ws.onmessage = (event) => {
+      setMessage(event.data)
     };
 
     // axios.get('http://localhost:8080/lobby/create').then((response) => {
@@ -38,38 +26,36 @@ class App extends React.Component<any, {message: string}> {
     //     message: response.data
     //   })
     // });
-  }
+  });
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <p>{this.state.message}</p>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>{message}</p>
 
-          <input
-            ref={this.inputRef}
-            type="text"
-          />
-          <br />
-          <button onClick={this.handleClick}>Submit</button>
-          <br />
+        <input
+          ref={inputRef}
+          type="text"
+        />
+        <br />
+        <button onClick={handleClick}>Submit</button>
+        <br />
 
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
