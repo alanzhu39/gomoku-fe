3e39be2798426ec4452ec9df82c373d274a053d2
Alan Zhu <alan.zhu39@gmail.com>
2022-11-22 15:36:48 -0800

    adding menu bar skeleton

--- src/MenuBar/MenuBar.tsx
+++ src/MenuBar/MenuBar.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import './MenuBar.css';
 
 function MenuBar() {
-  return <div className='MenuBar'>test</div>;
+  return (
+    <div className='MenuBar'>
+      <div className='MenuBarItem noHover'>PlayGomoku.net</div>
+      <div className='spacer'></div>
+      <div className='MenuBarItem'>About</div>
+      <div className='MenuBarItem'>How to play</div>
+      <div className='MenuBarItem'>
+        <img
+          src={require('../assets/GitHub-Logo.png')}
+          className='GitHubLogo'
+        />
+      </div>
+    </div>
+  );
 }
 
 export default MenuBar;
