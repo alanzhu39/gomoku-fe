e64931355756084095cec93b3f882e8605641b3a
Alan Zhu <alan.zhu39@gmail.com>
2022-11-22 22:19:13 -0800

    adding home button to clear state

--- src/MenuBar/MenuBar.tsx
+++ src/MenuBar/MenuBar.tsx
@@ -4,6 +4,16 @@ import './MenuBar.css';
 function MenuBar(props: { panes: any; setPanes: any }) {
   const githubUrl = 'https://github.com/alanzhu39/gomoku-fe';
 
+  function goHome() {
+    // Clear local storage
+    window.localStorage.removeItem('MOVES_LIST');
+    window.localStorage.removeItem('LOBBY_STATE');
+
+    // Redirect
+    const siteUrl = process.env.REACT_APP_SITE_URL!;
+    window.location.href = siteUrl;
+  }
+
   function openAboutPane() {
     props.setPanes({ aboutPane: true });
   }
@@ -14,7 +24,9 @@ function MenuBar(props: { panes: any; setPanes: any }) {
 
   return (
     <div className='MenuBar'>
-      <div className='MenuBarItem noHover'>PlayGomoku.net</div>
+      <div className='MenuBarItem' onClick={goHome}>
+        PlayGomoku.net
+      </div>
       <div className='spacer'></div>
       <div className='MenuBarItem' onClick={openAboutPane}>
         About
